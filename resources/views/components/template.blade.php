170aa1040f6304004cc5e57b0b979c29edec6123
RizkyMaulana310807 <rizkymaulana317b@gmail.com>
2025-08-05 21:29:22 +0700

    Menambahkan icon di tab

--- resources/views/components/template.blade.php
+++ resources/views/components/template.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
+    <link rel="icon" href="{{ asset('images/Logo1.png') }}" type="image/x-icon" />
+
     {{-- <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
     <!-- Coolvetica via CDN alternatif (karena tidak tersedia di Google Fonts) -->
